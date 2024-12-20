dca478e1fe40d0a1fcec816793e54a32ab46e09e
Tahreem Shaikh <tahreemshaikh118@gmail.com>
2024-12-20 14:42:44 +0530

    Create List_sum.py

---
+++ Assignment/List/List_sum.py
@@ -0,0 +1,11 @@
+my_list = [10, 20, 30, 40, 50]
+
+# variable to store the sum
+total = 0
+
+# Iterate through the list and add each element to the total
+for number in my_list:
+    total += number
+
+# Print the result
+print(f"The sum of the list is: {total}")
